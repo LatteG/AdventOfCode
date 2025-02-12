a98cf0ae194c424c8fd490848b2dcd9d191b1ecf
LatteG <gustav.lahti@gmail.com>
2025-02-12 10:56:01 +0100

    Solve day 16 part 2 2024

--- aoc_2024_rust/src/day16.rs
+++ aoc_2024_rust/src/day16.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
+use std::{collections::HashSet, fmt::Debug};
 
-#[derive(Clone, Copy, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 enum Tile {
     Empty,
     Wall,
@@ -12,11 +12,11 @@ enum Tile {
 impl Debug for Tile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Tile::Empty => write!(f, "."),
-            Tile::Wall  => write!(f, "#"),
+            Tile::Empty => write!(f, " "),
+            Tile::Wall  => write!(f, "."),
             Tile::Start => write!(f, "S"),
             Tile::End   => write!(f, "E"),
-            Tile::Visited(Reindeer { pos: _, score: _, facing: dir }) => write!(f, "{:?}", dir),
+            Tile::Visited(Reindeer { facing: dir, .. }) => write!(f, "{:?}", dir),
         }
     }
 }
@@ -69,11 +69,18 @@ impl Debug for Direction {
     }
 }
 
-#[derive(Clone, Copy, PartialEq, Eq, Debug, PartialOrd)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 struct Reindeer {
     pos: Coord,
     score: i32,
-    facing: Direction
+    facing: Direction,
+    path: HashSet<Coord>
+}
+
+impl PartialOrd for Reindeer {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 impl Ord for Reindeer {
@@ -96,35 +103,44 @@ impl Ord for Reindeer {
     }
 }
 
+static  mut PATH_CLONES: usize = 0;
+
 impl Reindeer {
     fn step(self, mut labyrinth: Labyrinth) -> (Vec<Reindeer>, Labyrinth) {
-        if self.has_finished(&labyrinth) {
+        if is_goal(&self.pos, &labyrinth) {
             (vec![self], labyrinth)
         } else {
             let mut reindeers: Vec<Reindeer> = Vec::new();
+            let (x, y): Coord = self.pos;
             for dir in [self.facing.get_counterclockwise(), self.facing, self.facing.get_clockwise()] {
                 let (dx, dy): Coord = dir.get_delta_coord();
-                match labyrinth[(self.pos.1 + dy) as usize][(self.pos.0 + dx) as usize] {
-                    Tile::End | Tile::Empty => reindeers.push(Reindeer{
-                        pos: (self.pos.0 + dx, self.pos.1 + dy),
-                        score: self.score + if dir == self.facing {1} else {1001},
-                        facing: dir,
-                    }),
-                    Tile::Visited(Reindeer { pos: _, score: old_score, facing: _ }) if old_score > self.score => reindeers.push(Reindeer{
-                        pos: (self.pos.0 + dx, self.pos.1 + dy),
-                        score: self.score + if dir == self.facing {1} else {1001},
-                        facing: dir,
-                    }),
-                    _ => (),
+                let next_pos @ (nx, ny): Coord = (x + dx, y + dy);
+                let next_score: i32 = self.score + if dir == self.facing {1} else {1001};
+
+                match labyrinth[ny as usize][nx as usize] {
+                    Tile::Wall | Tile::Start => (),
+                    Tile::Visited(Reindeer{score: old_score, ..}) if old_score < next_score => (),
+                    _ => {
+                        let mut next_path: HashSet<Coord> = self.path.clone();
+                        next_path.insert(next_pos);
+                        reindeers.push(Reindeer{
+                            pos: next_pos,
+                            score: next_score,
+                            facing: dir,
+                            path: next_path,
+                        })
+                    },
                 }
             }
-            labyrinth[self.pos.1 as usize][self.pos.0 as usize] = Tile::Visited(self);
+            if let Tile::Visited(Reindeer { score: old_score, .. }) = labyrinth[y as usize][x as usize] {
+                if old_score > self.score {
+                    labyrinth[y as usize][x as usize] = Tile::Visited(self);
+                }
+            } else {
+                labyrinth[y as usize][x as usize] = Tile::Visited(self);
+            }
             (reindeers, labyrinth)
         }
-    }
-
-    fn has_finished(self, labyrinth: &Labyrinth) -> bool {
-        labyrinth[self.pos.1 as usize][self.pos.0 as usize] == Tile::End
     }
 }
 
@@ -133,26 +149,51 @@ type Labyrinth = Vec<Vec<Tile>>;
 
 pub fn task1(input:&str) {
     let (labyrinth, reindeer): (Labyrinth, Reindeer) = parse_input(input);
-    let best_reindeer: Reindeer = run_through_maze(reindeer, labyrinth);
+    let (best_reindeer, _): (Reindeer, Vec<Reindeer>) = run_through_maze(reindeer, labyrinth);
     println!("Lowest score for labyrinth is: {}", best_reindeer.score);
 }
 
 pub fn task2(input:&str) {
-    println!("TODO: Task 2")
+    let (mut labyrinth, reindeer): (Labyrinth, Reindeer) = parse_input(input);
+    let (best_reindeer, reindeers): (Reindeer, Vec<Reindeer>) = run_through_maze(reindeer, labyrinth.clone());
+    let end_pos @ (best_x, best_y): Coord = best_reindeer.pos;
+    let best_tiles: HashSet<Coord> = find_best_tiles(best_reindeer, reindeers);
+    println!("There are {} tiles along the optimal paths", best_tiles.len());
 }
 
-fn run_through_maze(reindeer:Reindeer, mut labyrinth:Labyrinth) -> Reindeer {
+fn is_goal((x, y):&Coord, labyrinth:&Labyrinth) -> bool {
+    labyrinth[*y as usize][*x as usize] == Tile::End
+}
+
+fn print_score(labyrinth:&Labyrinth, (x, y):Coord) {
+    if let Tile::Visited(Reindeer { score, .. }) = labyrinth[y as usize][x as usize] {
+        println!("Score at ({}, {}): {}", x, y, score);
+    }
+}
+
+fn find_best_tiles(Reindeer{pos: goal_pos, score: best_score, path: mut coords, ..}:Reindeer, reindeers:Vec<Reindeer>) -> HashSet<Coord> {
+    for Reindeer { pos, score, facing: _, path } in reindeers {
+        if pos == goal_pos {
+            if score == best_score {
+                coords = coords.union(&path).copied().collect()
+            }
+        }
+    }
+    coords
+}
+
+fn run_through_maze(reindeer:Reindeer, mut labyrinth:Labyrinth) -> (Reindeer, Vec<Reindeer>) {//Labyrinth) {
     let mut reindeers: Vec<Reindeer> = vec![reindeer];
-    while reindeers.iter().any(|rd| !rd.has_finished(&labyrinth)) {
+    while reindeers.iter().any(|Reindeer {pos, ..}| !is_goal(pos, &labyrinth)) {
         let mut reindeer_acc: Vec<Reindeer> = Vec::new();
         for rd in reindeers {
             let mut temp: Vec<Reindeer> = Vec::new();
             (temp, labyrinth) = rd.step(labyrinth);
             reindeer_acc.append(&mut temp);
         }
-        reindeers = reindeer_acc.clone();
+        reindeers = reindeer_acc;
     }
-    *reindeers.iter().reduce(|min_rd, rd| min_rd.min(rd)).unwrap()
+    (reindeers.iter().reduce(|min_rd, rd| min_rd.min(rd)).unwrap().clone(), reindeers)
 }
 
 fn parse_input(input:&str) -> (Labyrinth, Reindeer) {
@@ -171,6 +212,7 @@ fn parse_input(input:&str) -> (Labyrinth, Reindeer) {
         pos: (x, y),
         score: 0,
         facing: Direction::East,
+        path: vec![(x, y)].into_iter().collect(),
     };
 
     (labyrinth, reindeer)
